47d0c8a5f2b97196ae667fc125d961707296cf24
Dominic Whelan <dominic.whelan27@gmail.com>
2022-12-11 14:28:51 -0330

    Initial commit

---
+++ src/main/java/com/keyin/algorithms/Node.java
@@ -0,0 +1,14 @@
+package com.keyin.algorithms;
+
+public class Node {
+
+    private int value;
+    private Node left;
+    private Node right;
+
+    public Node(int value){
+        this.value = value;
+        left = null;
+        right = null;
+    }
+}
